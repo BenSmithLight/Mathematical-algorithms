c297a969eb4ceb399d0ac85b20170648a0bf7838
LiuGuang <1207792390@qq.com>
2023-05-15 21:47:56 +0800

    新增README文件，将该项目开源至GitHub

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mathematical-algorithms
+匈牙利算法、最大流问题和单纯形法求解线性规划
--- 最大流问题/Matlab/model2.m
+++ 最大流问题/Matlab/model2.m
@@ -3,34 +3,36 @@ clc;
 clear;
 
 %% 获取用户输入并进行格式转换
-[cost_matrix, s, t, weights] = get_input();
+[costMatrix, source, target, weights] = get_input();
 
 %% 开始计算
-n = max(t);
+n = max(target);
 
-G = digraph(s, t, weights);
-plot(G, 'EdgeLabel', G.Edges.Weight, 'Layout', 'layered');
+% 创建有向图，从源点到汇点，权重为用户输入的权重
+graph = digraph(source, target, weights);
 
-[mf, ~, cs, ct] = maxflow(G, 1, n, "augmentpath");
-H = plot(G, 'Layout', 'layered', 'Sources', cs, 'Sinks', ct, ...
-    'EdgeLabel', G.Edges.Weight);
-highlight(H, cs, 'NodeColor', 'red')
-highlight(H, ct, 'NodeColor', 'green')
-disp(['最大流：', num2str(mf)]);
-endnodes = G.Edges.EndNodes;
+% 使用Ford-Fulkerson算法求解最大流
+[maxFlow, ~, cutSource, cutTarget] = maxflow(graph, 1, n, "augmentpath");
+
+%% 输出结果
+disp(['最大流：', num2str(maxFlow)]);
+
+% 输出最小割
+endNodes = graph.Edges.EndNodes;
 disp('最小割：')
 
-for i = 1:length(ct)
+% # 逐个检查节点，找到所有与源点连通的节点
+for i = 1:length(cutTarget)
 
-    if (find(endnodes(:, 2) == ct(i)))
-        [x, y] = find(endnodes(:, 2) == ct(i));
+    if (find(endNodes(:, 2) == cutTarget(i)))
+        [x, y] = find(endNodes(:, 2) == cutTarget(i));
 
         for j = 1:length(x)
 
-            if (find(ct == endnodes(x(j), 1)))
-
+            if (find(cutTarget == endNodes(x(j), 1)))
             else
-                disp([num2str(endnodes(x(j), 1)), '-->', num2str(endnodes(x(j), 2))]);
+                % 如果邻居节点不在集合A中，说明这条边是从集合A到集合A之外的边
+                disp([num2str(endNodes(x(j), 1)), '-->', num2str(endNodes(x(j), 2))]);
             end
 
         end
